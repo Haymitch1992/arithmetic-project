9da0a14afedbb96d6dc3ab94709fcd38a6b257af
003960 <wangzhiwei3306@163.com>
2020-12-31 09:13:17 +0800

    修改格式

--- plugin/index.js
+++ plugin/index.js
@@ -1,14 +1,14 @@
-import SVGContent from './dagContent'
-import NodeBus from './nodeBus'
+import SVGContent from './dagContent';
+import NodeBus from './nodeBus';
 const DAGBoard = {
   install(Vue, options) {
-    Vue.component(SVGContent.name, SVGContent)
-    Vue.component(NodeBus.name, NodeBus)
+    Vue.component(SVGContent.name, SVGContent);
+    Vue.component(NodeBus.name, NodeBus);
   }
-}
+};
 
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(DAGBoard)
+  window.Vue.use(DAGBoard);
 }
 
-export default DAGBoard
+export default DAGBoard;
