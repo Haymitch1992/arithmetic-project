39ac0b372b74beb308aef45991e6c0a21733f740
003960 <wangzhiwei3306@163.com>
2021-01-15 18:04:49 +0800

    增加帮助中心模块

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -22,94 +22,99 @@
                 <el-menu-item index="5">
                     <span slot="title">模板部署</span>
                 </el-menu-item>
+                <el-menu-item index="6">
+                    <span slot="title">帮助中心</span>
+                </el-menu-item>
             </el-menu>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        methods: {
-            handleSelect(key, keyPath) {
-                // 根据index 判断页面跳转
-                let index = keyPath[keyPath.length - 1];
-                switch (index) {
-                    case '1':
-                        this.$router.push('/plan');
-                        break;
-                    case '2':
-                        this.$router.push('/dataManagement');
-                        break;
-                    // case '2-2':
-                    //     this.$router.push('/dataTable') // 废弃
-                    //     break
-                    case '3':
-                        this.$router.push('/projectManagement');
-                        break;
-                    case '3-2':
-                        this.$router.push('/engineering');
-                        break;
-                    case '3-3':
-                        this.$router.push('/selectAlgorithm');
-                        break;
-                    case '3-4':
-                        this.$router.push('/assessAlgorithm');
-                        break;
-                    case '4':
-                        this.$router.push('/modelManagement');
-                        break;
-                    case '5':
-                        this.$router.push('/online');
-                        break;
-                }
+export default {
+    methods: {
+        handleSelect(key, keyPath) {
+            // 根据index 判断页面跳转
+            let index = keyPath[keyPath.length - 1];
+            switch (index) {
+                case '1':
+                    this.$router.push('/plan');
+                    break;
+                case '2':
+                    this.$router.push('/dataManagement');
+                    break;
+                // case '2-2':
+                //     this.$router.push('/dataTable') // 废弃
+                //     break
+                case '3':
+                    this.$router.push('/projectManagement');
+                    break;
+                case '3-2':
+                    this.$router.push('/engineering');
+                    break;
+                case '3-3':
+                    this.$router.push('/selectAlgorithm');
+                    break;
+                case '3-4':
+                    this.$router.push('/assessAlgorithm');
+                    break;
+                case '4':
+                    this.$router.push('/modelManagement');
+                    break;
+                case '5':
+                    this.$router.push('/online');
+                    break;
+                case '6':
+                    this.$router.push('/help');
+                    break;
             }
         }
-    };
+    }
+};
 </script>
 
 <style scoped lang="scss">
-    .menu-container{
-        padding-top: 50px;
-        background-color: #fff;
-        border-top-right-radius: 50px;
-        box-shadow: 1px 1px 4px rgba(0,0,0,.2);
-        height: calc(100vh - 70px);
-        box-sizing: border-box;
-        overflow: hidden;
-    }
-    .system-logo{
-        width: 24px;
-        margin-right: 6px;
-        vertical-align: sub;
-    }
-    .menu-title{
-        padding-left: 20px;
-        color: #fff;
-        font-size: 18px;
-        color: #464F68;
-        line-height: 70px;
-        margin: 0;
-    }
-
+.menu-container {
+    padding-top: 50px;
+    background-color: #fff;
+    border-top-right-radius: 50px;
+    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.2);
+    height: calc(100vh - 70px);
+    box-sizing: border-box;
+    overflow: hidden;
+}
+.system-logo {
+    width: 24px;
+    margin-right: 6px;
+    vertical-align: sub;
+}
+.menu-title {
+    padding-left: 20px;
+    color: #fff;
+    font-size: 18px;
+    color: #464f68;
+    line-height: 70px;
+    margin: 0;
+}
 </style>
 <style>
-    .menu-box .el-menu{
-        border-right: 0;
-    }
-    .menu-box .el-menu-item{
-        height: 36px;
-        line-height: 36px;
-        margin: 16px 10px;
-        transition: none;
-    }
-    .menu-box .el-menu-item:focus, .menu-box .el-menu-item:hover{
-        background: transparent;
-    }
-    .menu-box .el-menu-item.is-active{
-        background: #1677FF;
-        color: #fff;
-        border-radius: 40px;
-        box-shadow: 2px 0 6px 0 rgba(172, 170, 170, 0.6);
-
-    }
+.menu-box .el-menu {
+    border-right: 0;
+}
+.menu-box .el-menu-item {
+    height: 36px;
+    line-height: 36px;
+    margin: 16px 10px;
+    transition: none;
+}
+.menu-box .el-menu-item:focus,
+.menu-box .el-menu-item:hover {
+    background: transparent;
+}
+.menu-box .el-menu-item.is-active {
+    background: #1677ff;
+    color: #fff;
+    border-radius: 40px;
+    box-shadow: 2px 0 6px 0 rgba(172, 170, 170, 0.6);
+}
 </style>
---
+++ src/views/help/help.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="bg">
+        <div class="online-box">
+            <h3>算法中台说明文档</h3>
+            <p>1 产品介绍</p>
+            <p>2 数据准备</p>
+            <p>3 模型开发和训练</p>
+            <p>3.1 入门介绍</p>
+            <p>3.2 数据预处理（拖拽生成节点树）</p>
+            <p>3.3 配置参数</p>
+            <p>3.4 运行实验</p>
+            <p>3.5 数据可视化</p>
+            <p>3.6 常规学习组件</p>
+            <p>3.6.1 数据预处理</p>
+            <p>3.6.2 特征工程</p>
+            <p>4 模型管理</p>
+            <p>5 模型部署</p>
+            <p>6 常见问题</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'help'
+};
+</script>
+
+<style scoped lang="scss">
+.bg {
+    padding: 20px;
+}
+.online-box {
+    height: calc(100vh - 100px);
+    background-color: #fff;
+    border-top-left-radius: 50px;
+    padding: 30px 30px 20px;
+    box-sizing: border-box;
+    box-shadow: 1px 2px 3px #e5e5e5;
+    .pagination-line {
+        background-color: #fff;
+        padding: 20px 20px 10px;
+        text-align: right;
+    }
+    .model-status {
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        display: inline-block;
+        background-color: #666666;
+        vertical-align: middle;
+        margin-right: 6px;
+    }
+    .model-status-0 {
+        background-color: #1677ff;
+    }
+    .model-status-1 {
+        background-color: #dddddd;
+    }
+    .model-status-2 {
+        background-color: #52c41a;
+    }
+    .model-status-3 {
+        background-color: #ff3b30;
+    }
+    .model-status-4 {
+        background-color: #ebad18;
+    }
+}
+</style>
