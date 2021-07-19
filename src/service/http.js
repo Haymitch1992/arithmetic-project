47992be7a449fbc04bd729c7c99ed3c6540c0fbf
003960 <wangzhiwei3306@163.com>
2021-07-19 18:05:37 +0800

    过滤接口

--- src/service/http.js
+++ src/service/http.js
@@ -12,10 +12,16 @@ const service = axios.create({
 // request interceptor
 service.interceptors.request.use(
     config => {
-        // 如果 data_user_token 和  data_user_id 是空则跳转至登录
-        if ((!localStorage.getItem('data_user_token')) || (!localStorage.getItem('data_user_id'))) {
-            location.href = '#/login';
+        let arr = ['/api/queryEmail/'];
+        // 需要过滤的接口
+        if (arr.includes(config.url)) {
+            console.log('接口过滤');
+        } else {
+            if ((!localStorage.getItem('data_user_token')) || (!localStorage.getItem('data_user_id'))) {
+                location.href = '#/login';
+            }
         }
+        // 如果 data_user_token 和  data_user_id 是空则跳转至登录
         // do something before request is sent
         config.headers["usertoken"] = localStorage.getItem('data_user_token');
         config.headers["datauserid"] = localStorage.getItem('data_user_id');
